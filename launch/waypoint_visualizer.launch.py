9460e3e4148743a2e086ffc50d55cc45b8836155
fuka <294387332+fuka0511@users.noreply.github.com>
2026-09-02 16:51:36 +0900

    パスを引数に変更

--- launch/waypoint_visualizer.launch.py
+++ launch/waypoint_visualizer.launch.py
@@ -1,18 +1,31 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    # 1. コマンドラインから受け取る引数を宣言
+    target_file_arg = DeclareLaunchArgument(
+        'target_file',
+        default_value='',
+        description='CSVファイルの絶対パス'
+    )
+
+    # 2. 引数を参照する変数を定義
+    target_file = LaunchConfiguration('target_file')
+
+    # 3. ノードの設定
+    waypoint_visualizer_node = Node(
+        package='oit_robot_utils',
+        executable='waypoint_visualizer.py',
+        name='waypoint_visualizer',
+        output='screen',
+        parameters=[{
+            'csv_path': target_file
+        }]
+    )
 
     return LaunchDescription([
-        Node(
-            package='oit_robot_utils',
-            executable='waypoint_visualizer.py',
-            name='waypoint_visualizer',
-            output='screen',
-            parameters=[
-                {
-                    'csv_path': '/home/ubuntu/ros2_ws/src/oit_navigation/csv/points.csv'
-                }
-            ]
-        )
+        target_file_arg,
+        waypoint_visualizer_node
     ])
